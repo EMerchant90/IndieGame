9c72809546e3fe09e41295ae962c498703bd692b
EMerchant90 <ejaz.merchant@icloud.com>
2018-05-14 01:55:22 -0400

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement() {
+        if (Input.GetKey(KeyCode.W))
+        { transform.Translate(0, speed * Time.deltaTime, 0); }
+        else if (Input.GetKey(KeyCode.S))
+        { transform.Translate(0, -speed * Time.deltaTime, 0); }
+        else if (Input.GetKey(KeyCode.A))
+        { transform.Translate(-speed * Time.deltaTime, 0, 0); }
+        else if (Input.GetKey(KeyCode.D))
+        { transform.Translate(speed * Time.deltaTime, 0, 0); }
+            
+    }
+}
