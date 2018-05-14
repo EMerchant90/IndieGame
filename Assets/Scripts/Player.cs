98f5645efc57d3b63fbe969398411064312390fd
EMerchant90 <ejaz.merchant@icloud.com>
2018-05-14 03:00:09 -0400

    Added character and animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour {
 
 
     public float speed;
+    Animator anim;
 	// Use this for initialization
 	void Start () {
+        anim = GetComponent<Animator>();
 		
 	}
 	
@@ -18,13 +20,15 @@ public class Player : MonoBehaviour {
 
     void Movement() {
         if (Input.GetKey(KeyCode.W))
-        { transform.Translate(0, speed * Time.deltaTime, 0); }
+        { transform.Translate(0, speed * Time.deltaTime, 0); anim.SetInteger("dir", 0); anim.speed = 1; }
         else if (Input.GetKey(KeyCode.S))
-        { transform.Translate(0, -speed * Time.deltaTime, 0); }
+        { transform.Translate(0, -speed * Time.deltaTime, 0); anim.SetInteger("dir", 1); anim.speed = 1; }
         else if (Input.GetKey(KeyCode.A))
-        { transform.Translate(-speed * Time.deltaTime, 0, 0); }
+        { transform.Translate(-speed * Time.deltaTime, 0, 0); anim.SetInteger("dir", 2); anim.speed = 1; }
         else if (Input.GetKey(KeyCode.D))
-        { transform.Translate(speed * Time.deltaTime, 0, 0); }
+        { transform.Translate(speed * Time.deltaTime, 0, 0); anim.SetInteger("dir", 3); anim.speed = 1; }
+        else
+            anim.speed = 0;
             
     }
 }
